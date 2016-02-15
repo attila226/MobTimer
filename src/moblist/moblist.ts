1329b5fe9c24960a6c78efae5093229e52a6cdbc
Attila Buturla <AttilaButurla@gmail.com>
2016-02-14 21:10:48 -0800

    WIP

--- src/moblist/moblist.ts
+++ src/moblist/moblist.ts
@@ -1,11 +1,22 @@
-import {Component, View, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/core';
+import {Component, View } from 'angular2/core';
+import {COMMON_DIRECTIVES} from 'angular2/common';
 
 @Component({
   selector: 'moblist',
-  templateUrl: './src/moblist/moblist.html' 
+  templateUrl: './src/moblist/moblist.html',
+  directives: [COMMON_DIRECTIVES]
 })
 
 export class Moblist {
-list =  ['Bevis', 'Butthead', 'Jerkface'];    
+    list: string[] =  ['Bevis', 'Butthead', 'Jerkface'];    
     
- }
+    move(idx: number, step: number) {
+        var tmp = this.list[idx];
+        this.list[idx] = this.list[idx - step];
+        this.list[idx - step] = tmp;
+    }
+  
+    remove(idx: number) {
+	   this.list.splice(idx, 1); 
+    }   
+}
